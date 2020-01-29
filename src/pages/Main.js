9059fbed8bd5c020d019252b41f0749c299de97f
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-01-29 01:56:28 -0300

    configurated webview

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { getCurrentPositionAsync, requestPermissionsAsync } from 'expo-location';
 
-function Main() {
+function Main({ navigation }) {
 
 	const [currentRegion, setCurrentRegion] = useState(null);
 	useEffect(() => {
 		async function loadInitialLocation() {
 			const { granted } = await requestPermissionsAsync();
 
-			if(granted) {
+			if (granted) {
 				const { coords } = await getCurrentPositionAsync({
 					enableHighAccuracy: true
 				});
@@ -34,12 +34,46 @@ function Main() {
 		return null;
 	}
 
-	return <MapView initialRegion={currentRegion} style={styles.map} />
+	return <MapView initialRegion={currentRegion} style={styles.map} >
+		<Marker coordinate={{ latitude: -29.891507,longitude: -50.2577158 }}>
+			<Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/7891544?s=460&v=4'}} />
+			<Callout onPress={ () => {
+				navigation.navigate('Profile', { github_username: 'viniciusrosa' });
+			} }>
+				<View style={styles.callout} >
+					<Text style={styles.devName}>Vinicius rosa</Text>
+					<Text style={styles.devBio}>Biografia</Text>
+					<Text style={styles.devTechs}>tecnologias</Text>
+				</View>
+			</Callout>
+		</Marker>
+	</MapView>
 }
 
 const styles = StyleSheet.create({
 	map: {
 		flex: 1
+	},
+	avatar: {
+		width: 54,
+		height: 54,
+		borderRadius: 4,
+		borderWidth: 4,
+		borderColor: '#fff'
+	},
+	callout: {
+		width: 260,
+	},
+	devName: {
+		fontWeight: 'bold',
+		fontSize: 16
+	},
+	devBio: {
+		color: '#666',
+		marginTop: 5
+	},
+	devTechs: {
+		marginTop: 5
 	}
 })
 
